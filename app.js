d42b12764709b323d1ee1016334a215b0f0baa1b
Abdul qadeer <102538683+qadeer77@users.noreply.github.com>
2022-08-08 11:48:28 +0500

    js-file-created

---
+++ app.js
@@ -0,0 +1,293 @@
+var mobilePhones = {
+    // iphone
+    iphone: {
+        iphone6: {
+            name: "Iphone 6",
+            brand: "Apple",
+            colors: ["red", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-6-4.jpg",
+            isPta: false,
+        },
+        iphone6s: {
+            name: "Iphone 6s",
+            brand: "Apple",
+            colors: ["pink", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-6s1.jpg",
+            isPta: false,
+        },
+        iphone6Plus: {
+            name: "Iphone 6 plus",
+            brand: "Apple",
+            colors: ["gray", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-6-plus2.jpg",
+            isPta: false,
+        },
+        iphone6sPlus: {
+            name: "Iphone 6s plus",
+            brand: "Apple",
+            colors: ["pink", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-6s-plus.jpg",
+            isPta: false,
+        },
+        iphone13: {
+            name: "Iphone 13",
+            brand: "Apple",
+            colors: ["blue", "bluewhite"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13.jpg",
+            isPta: false,
+        },
+        iphone13Pro: {
+            name: "Iphone 13 pro",
+            brand: "Apple",
+            colors: ["gray", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro.jpg",
+            isPta: false,
+        },
+        iphone13Mini: {
+            name: "Iphone 13 mini",
+            brand: "Apple",
+            colors: ["pink", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg",
+            isPta: false,
+        },
+        iphone13ProMax: {
+            name: "Iphone 13 pro max",
+            brand: "Apple",
+            colors: ["gray", "blue"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro-max.jpg",
+            isPta: false,
+        },
+        iphoneSe: {
+            name: "Iphone se",
+            brand: "Apple",
+            colors: ["red", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-se-2020.jpg",
+            isPta: false,
+        },
+        iphone7: {
+            name: "Iphone 7",
+            brand: "Apple",
+            colors: ["pink", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-7r4.jpg",
+            isPta: false,
+        },
+        iphone7Plus: {
+            name: "Iphone 7 plus",
+            brand: "Apple",
+            colors: ["black", "gray"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-7-plus-r2.jpg",
+            isPta: false,
+        },
+        iphone12: {
+            name: "Iphone 12",
+            brand: "Apple",
+            colors: ["blue", "bluewhite"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12.jpg",
+            isPta: false,
+        },
+        iphone12Pro: {
+            name: "Iphone 12 pro",
+            brand: "Apple",
+            colors: ["gray", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro--.jpg",
+            isPta: false,
+        },
+        iphone12Mini: {
+            name: "Iphone 12 mini",
+            brand: "Apple",
+            colors: ["gray", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-mini.jpg",
+            isPta: false,
+        },
+        iphone12ProMax: {
+            name: "Iphone 12 pro max",
+            brand: "Apple",
+            colors: ["bluewhite", "blue"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro-max-.jpg",
+            isPta: false,
+        },
+        iphoneX: {
+            name: "Iphone X",
+            brand: "Apple",
+            colors: ["black", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-x.jpg",
+            isPta: false,
+        },
+        iphoneXR: {
+            name: "Iphone XR",
+            brand: "Apple",
+            colors: ["orange", "red"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-xr-new.jpg",
+            isPta: false,
+        },
+        iphoneXS: {
+            name: "Iphone XS",
+            brand: "Apple",
+            colors: ["pink", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-xs-new.jpg",
+            isPta: false,
+        },
+        iphoneXSMax: {
+            name: "Iphone XS Max",
+            brand: "Apple",
+            colors: ["pink", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-xs-max-new1.jpg",
+            isPta: false,
+        },
+    },
+    // samsung
+    samsung: {
+        A235G: {
+            name: "Samsung Galaxy A23 5G",
+            brand: "Samsung",
+            colors: ["pink", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a23-5g.jpg",
+            isPta: true,
+        },
+        M135G: {
+            name: "Samsung Galaxy M13 5G",
+            brand: "Samsung",
+            colors: ["white", "Brown"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-m13-5g.jpg",
+            isPta: true,
+        },
+        M13: {
+            name: "Samsung Galaxy M13",
+            brand: "Samsung",
+            colors: ["blue", "bluewhite"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-m13-4g-india.jpg",
+            isPta: true,
+        },
+        A13: {
+            name: "Samsung Galaxy A13 (SM-A137)",
+            brand: "Samsung",
+            colors: ["white", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a13-a137.jpg",
+            isPta: true,
+        },
+        Xcover6: {
+            name: "Samsung Galaxy Xcover 6",
+            brand: "Samsung",
+            colors: ["black", "black"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-xcover6-pro-r.jpg",
+            isPta: true,
+        },
+        F13: {
+            name: "Samsung Galaxy F13",
+            brand: "Samsung",
+            colors: ["white", "green"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-f13.jpg",
+            isPta: true,
+        },
+        S6: {
+            name: "Samsung Galaxy Tab S6 Lite",
+            brand: "Samsung",
+            colors: ["black", "gray"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/galaxy-tab-s6-lite-2022-lte-sm-p619-.jpg",
+            isPta: true,
+        },
+        S20: {
+            name: "Samsung Galaxy S20 FE",
+            brand: "Samsung",
+            colors: ["white", "pink"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s20-fe-5g.jpg",
+            isPta: true,
+        },
+        A73: {
+            name: "Samsung Galaxy A73 5G",
+            brand: "Samsung",
+            colors: ["gray", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a73-5g.jpg",
+            isPta: true,
+        },
+        A33: {
+            name: "Samsung Galaxy A33 5G",
+            brand: "Samsung",
+            colors: ["white", "pink"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a33-5g.jpg",
+            isPta: true,
+        },
+        F23: {
+            name: "Samsung Galaxy F23",
+            brand: "Samsung",
+            colors: ["blue", "bluewhite"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-m23.jpg",
+            isPta: true,
+        },
+        M33: {
+            name: "Samsung Galaxy M33",
+            brand: "Samsung",
+            colors: ["white", "green"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-m33.jpg",
+            isPta: true,
+        },
+        S22: {
+            name: "Samsung Galaxy S22 Ultra 5G",
+            brand: "Samsung",
+            colors: ["black", "brown"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s22-ultra-5g.jpg",
+            isPta: true,
+        },
+        S21: {
+            name: "Samsung Galaxy S21 FE 5G",
+            brand: "Samsung",
+            colors: ["white", "gray"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s21-fe-5g.jpg",
+            isPta: true,
+        },
+        F42: {
+            name: "Samsung Galaxy F42",
+            brand: "Samsung",
+            colors: ["black", "blue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-f42-5g.jpg",
+            isPta: true,
+        },
+        M51: {
+            name: "Samsung Galaxy M51",
+            brand: "Samsung",
+            colors: ["pink", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-m51.jpg",
+            isPta: true,
+        },
+        Note20: {
+            name: "Samsung Galaxy Note20 5G",
+            brand: "Samsung",
+            colors: ["white", "green"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-note20-5g-r.jpg",
+            isPta: true,
+        },
+        Fold2: {
+            name: "Samsung Galaxy Z Fold2 5G",
+            brand: "Samsung",
+            colors: ["white", "pink"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-z-fold2-5g.jpg",
+            isPta: true,
+        },
+        A42: {
+            name: "Samsung Galaxy A42 5G",
+            brand: "Samsung",
+            colors: ["pink", "black"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a42-5g.jpg",
+            isPta: true,
+        },
+    },
+}
